547135b10e41afbb9c9c0c45ee279d597fae9328
sofanit <sofanitmesfin19@gmail.com>
2024-10-23 22:19:55 +0300

    back end MVC setups

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
 import express from "express";
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.routes.js"
+import connectMongoDB from "./db/connectMongoDB.js"
+
+dotenv.config()
 const app=express();
 
-app.get(
-    "/",(req,res)=>{
-        res.send("Server is ready")
+const port=process.env.PORT || 5000
 
-    }
-)
+app.use("/api/auth",authRoutes);
 
-
-
-app.listen(8000,()=>{
-    console.log("server is running on port 8000")
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+    connectMongoDB();
 })
