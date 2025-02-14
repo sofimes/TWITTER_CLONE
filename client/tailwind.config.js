a4d920545f0bb17606d5e0505c5d0a1046086a3b
sofimes <sofanitmesfin19@gmail.com>
2025-02-14 10:44:37 +0300

    tailwindcss update

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,6 @@ import daisyui from "daisyui";
 import daisyUIThemes from "daisyui/src/theming/themes";
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
@@ -11,15 +10,12 @@ export default {
 
   daisyui: {
     themes: [
-      "black",
-
+      "light",
       {
         black: {
           ...daisyUIThemes["black"],
-          " primary": "rgb(29, 155, 240)",
-          " secondary": "rgb(24, 24, 24)",
-          " base-100": "#121212", // Background color (dark)
-          "base-content": "#ffffff",
+          primary: "rgb(29, 155, 240)",
+          secondary: "rgb(24, 24, 24)",
         },
       },
     ],
